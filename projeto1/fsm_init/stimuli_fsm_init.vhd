42a830c4c622e11ad30ec4b07eedb5679f43da26
João Pedro Sartório Lovatti <joaoplovatti@gmail.com>
2019-05-29 19:34:40 -0300

    Fsm init complete
    :smile:

--- projeto1/fsm_init/stimuli_fsm_init.vhd
+++ projeto1/fsm_init/stimuli_fsm_init.vhd
@@ -55,8 +55,73 @@ architecture stimuli_fsm_init_arc of stimuli_fsm_init is
 
     --Behavioral
     STIMULI : process is
-		--PROCEDURES
-    	begin
+			--PROCEDURES
+			procedure reset_active is
+				--GO TO RESET_ROW
+				begin
+
+					RES <= '1';
+					wait for 5*CLK_PERIOD;
+					RES <= '0';
+
+			end procedure reset_active;
+
+			procedure loop1 is
+				--RESET_ROW -> JUMP_ROW -> WRITE_HEAD -> READY -> RESET_ROW
+				begin
+
+					FSM_M_START <= '1';
+					OFC_OF_X <= '1';
+					OFC_OF_Y <= '1';
+					wait for 5*CLK_PERIOD;
+
+			end procedure loop1;
+
+			procedure loop2 is
+				--RESET_ROW -> JUMP_ROW -> RESET_ROW
+				begin
+
+					FSM_M_START <= '1';
+					OFC_OF_X <= '1';
+					OFC_OF_Y <= '0';
+					wait for 3*CLK_PERIOD;
+
+			end procedure loop2;
+
+			procedure loop3 is
+				--RESET_ROW -> RESET_ROW
+				begin
+
+					FSM_M_START <= '1';
+					OFC_OF_X <= '0';
+					OFC_OF_Y <= '1';
+					wait for 2*CLK_PERIOD;
+
+			end procedure loop3;
+
+			procedure loop4 is
+				--RESET_ROW -> JUMP_ROW -> WRITE_HEAD -> READY -> READY
+				begin
+
+					FSM_M_START <= '1';
+					OFC_OF_X <= '1';
+					OFC_OF_Y <= '1';
+					wait for 4*CLK_PERIOD;
+					FSM_M_START <= '0';
+					wait for CLK_PERIOD;
+
+			end procedure loop4;
+
+			begin
+
+				reset_active;
+				loop1;
+				reset_active;
+				loop2;
+				reset_active;
+				loop3;
+				reset_active;
+				loop4;
 
     end process STIMULI;
 
